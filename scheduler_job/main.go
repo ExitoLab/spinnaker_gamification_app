a32fb6c0a1d68c01fb009c5a6861ab605f29a2d4
Ige Adetokunbo Temitayo <igeadetokunbo@gmail.com>
2020-09-02 20:33:51 +0400

    Created boiler template
    Signed-off-by: Ige Adetokunbo Temitayo <igeadetokunbo@gmail.com>

--- scheduler_job/main.go
+++ scheduler_job/main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func main() {
-	resp, err := http.Get("curl -X GET -u ExitoLab:9d82da6de5c01a0ebacbd11752cba900a9dc4219 https://api.github.com/repos/ExitoLab/spinnaker-ms-teams-notification-plugin/issues")
+	resp, err := http.Get("curl -X GET -u  https://api.github.com/repos/ExitoLab/spinnaker-ms-teams-notification-plugin/issues")
 	if err != nil {
 		print(err)
 	}
